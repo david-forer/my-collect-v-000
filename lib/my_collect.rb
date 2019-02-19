1973e9b3304613bdc4d92d19d79ea4960c12bbe4
David Forer <david.j.forer@gmail.com>
2019-02-19 09:52:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,11 @@
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
+def my_collect(array)
+
+ 	i = 0
+	collection = []
+	while i < array.length
+    collection << yield(array[i])
+		i += 1
+	end
+	collection
 end
 
