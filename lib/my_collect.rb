479803a4dd895686a32871eb3e5ed6eedd43b08c
David Forer <david.j.forer@gmail.com>
2019-02-19 09:50:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,5 @@
-collection = ["main street", "first street", "second street"]
-
-my_collect(collection) do |street|
-  street.split(" ").first
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
 end
 
