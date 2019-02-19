a67a0f98c7aba3d3c5a2d76a134f91181b14dae2
David Forer <david.j.forer@gmail.com>
2019-02-19 09:49:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,6 @@
 collection = ["main street", "first street", "second street"]
 
-my_collect(collection) do |name|
-  name.split(" ").first
+my_collect(collection) do |street|
+  street.split(" ").first
 end
 
